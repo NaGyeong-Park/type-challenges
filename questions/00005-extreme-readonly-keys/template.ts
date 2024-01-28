a2e2f511043138c2eee35f7635e52ec61d442daf
NaGyeong Park <ngp.july@gmail.com>
2024-01-28 23:23:08 +0900

    00005

--- questions/00005-extreme-readonly-keys/template.ts
+++ questions/00005-extreme-readonly-keys/template.ts
@@ -1,1 +1,5 @@
-type GetReadonlyKeys<T> = any
+type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? A : B;
+
+type GetReadonlyKeys<T> = {
+  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { readonly [Q in P]: T[P] }, P>;
+}[keyof T];
